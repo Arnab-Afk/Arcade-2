b0b2591b4b15ef58f8afbd8f223885486f7cc8b4
Arnab Bhowmik <arnabbhowmik019@gmail.com>
2024-07-01 08:57:02 +0530

    initializers done

---
+++ initializers/connectToDb.go
@@ -0,0 +1,21 @@
+package initializers
+
+import (
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDb() {
+	var err error
+	dsn := os.Getenv("DSN")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		PrepareStmt: false,
+	})
+	if err != nil {
+		panic("failed to connect database")
+	}
+}
